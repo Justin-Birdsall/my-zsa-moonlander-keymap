ed983e70ee16d0454aa96a62c9335259d0528a65
Justin-Birdsall <jlb7487@gmail.com>
2026-06-11 01:43:49 +0000

    Force pool ripple mode at boot (EEPROM keeps old mode otherwise)

--- Jmpn7/keymap.c
+++ Jmpn7/keymap.c
@@ -89,6 +89,7 @@ RGB hsv_to_rgb_with_value(HSV hsv) {
 
 void keyboard_post_init_user(void) {
   rgb_matrix_enable();
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_POOL_RIPPLE);
 }
 
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
